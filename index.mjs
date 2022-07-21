f70f6bb2474e1ae1f92e2793c571e133d1ef5094
Roman Isaev <mdnight@riseup.net>
2022-07-21 16:09:00 +0300

    add example index.mjs

--- index.mjs
+++ index.mjs
@@ -1,11 +1,24 @@
-import * as dotenv from 'dotenv'
-import {initS3Client} from './lib/index.mjs';
+'use strict'
 
-dotenv.config();
+import * as dotenv from 'dotenv'
+import S3Client from './lib/client.mjs'
+import S3Fs from './lib/s3fs.mjs'
+import { createReadStream } from 'fs'
+import { fromEnv } from '@aws-sdk/credential-providers'
+import { env } from 'process'
+
+dotenv.config()
 
 const start = async () => {
-    const client = await initS3Client()
-    console.log(client.config.region());
+  const client = new S3Client(await fromEnv(), env.AWS_REGION, env.S3_BUCKET)
+  const s3fs = new S3Fs(client, '')
+
+  const dataStream = await createReadStream('./HUGE_FILE.zip')
+  const writeStream = await s3fs.createWriteStream('HUGE_FILE.zip')
+  await writeStream.start()
+  dataStream.pipe(writeStream)
+
+  await s3fs.rm('HUGE_FILE.zip')
 }
 
-start();
+start()
